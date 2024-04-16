3ff9fd7009745ce6c2a92de164915d03c984f568
ChandraShekhar <csyadav0513@gmail.com>
2024-04-16 13:03:05 +0530

    Add dotenv configuration files and dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,37 @@
+// Import required modules using ES Modules syntax
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import {PORT} from "./config.js";
 
+// Load environment variables from .env file
+dotenv.config({ path: "../.env" });
+
+// Extract PORT from environment variables
+
+
+// Create Express app
 const app = express();
 
+// Define a route
 app.get("/", (req, res) => {
   res.send("Hello World 🌍");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} 🚀`);
-});
+// Connect to MongoDB using Mongoose
+mongoose
+  .connect(`${process.env.MONGODB_URL}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connected successfully 🦄🚀");
+
+    // Start the Express server after successful MongoDB connection
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT} 🚀`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
